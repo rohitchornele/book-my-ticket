b8c96ec24a4ed056741e885879f307f4bf683828
Rohit Chornele <rohitchornele@gmail.com>
2026-04-16 02:05:29 +0530

    registration improved again

--- db/index.js
+++ db/index.js
@@ -1,13 +1,4 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import pg from 'pg';
 
-
-export const db = drizzle(
-    new pg.Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false,
-        },
-    })
-);
+export const db = drizzle(process.env.DATABASE_URL);
